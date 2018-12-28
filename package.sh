7c4386ec05d66509f2063bcaf7d0b71a90ca25c0
Oliveira <oliveira@joao.oliveira>
2018-12-28 21:26:50 -0200

    upload package to s3

--- package.sh
+++ package.sh
@@ -1,2 +1,4 @@
 npm install
-tar -czvf helloposworks-backend.tgz node_modules index.js package.json package-lock.json
+tar -czvf helloopsworks-backend.tgz node_modules index.js package.json package-lock.json
+
+aws s3 cp helloopsworks-backend.tgz s3://${AWS_BUCKET}/helloopsworks-backend.tgz
